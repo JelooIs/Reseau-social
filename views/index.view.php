dc3f1848790e05845524a61a871cea6d910eeda5
Dylan Lernout <dylanlernoutm.boa@gmail.com>
2025-09-16 10:24:13 +0200

    Liaison message/utilisateur

--- views/index.view.php
+++ views/index.view.php
@@ -14,23 +14,16 @@
     <?php endif; ?>
     <form method="post">
         <p>
-            <label for="nom">Nom:</label>
-            <input type="text" name="nom" id="nom" required="required"/> <br/>
-
-            <label for="prenoms">Prénoms</label>
-            <input type="text" name="prenoms" id="prenoms" required="required"/> <br/>
- 
             <label for="messages">Message:</label>
             <textarea type="text" name="message" id="messages" rows="5"></textarea> <br/>
-
             <input type="submit" name="envoyer" value="Envoyer"/>
         </p>
     </form>
-    <?php if (!empty($message)): ?>
+    <?php if (!empty($messages)): ?>
         <ul>
             <?php foreach ($messages as $msg): ?>
                 <li><strong><?= htmlspecialchars($msg['nom'] . ' ' . $msg['prenoms']) ?>:</strong> <?= htmlspecialchars($msg['message']) ?> <em>(<?= $msg['created_at'] ?>)</em></li>
-                <?php endforeach; ?>
+            <?php endforeach; ?>
         </ul>
         <?php else: ?>
             <p>Aucun message pour l'instant.</p>
